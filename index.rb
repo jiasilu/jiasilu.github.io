089772f2bf892a1b63d6bc1f0821bed16ebb23f3
claudiob <claudiob@gmail.com>
2013-10-24 14:12:32 -0700

    From claudiob to jiasilu

--- index.rb
+++ index.rb
@@ -5,12 +5,12 @@ require_relative 'lib/github_api_client'
 template = File.read 'lib/template.html.erb'
 erb = ERB.new template, 0, '<>'
 
-user = load_user 'claudiob'
+user = load_user 'jiasilu'
 
-repos = load_sources 'claudiob'
+repos = load_sources 'jiasilu'
 is_fork = false
 File.open('index.html', 'w') {|f| f.puts erb.result}
 
-repos = load_forks 'claudiob'
+repos = load_forks 'jiasilu'
 is_fork = true
 File.open('forks.html', 'w') {|f| f.puts erb.result}
